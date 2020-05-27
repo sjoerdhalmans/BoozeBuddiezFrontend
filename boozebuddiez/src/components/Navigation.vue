8f44f8aff3ea1d4148d4175b27f24297ddf39f1f
sjoerdhalmans <43236006+sjoerdhalmans@users.noreply.github.com>
2020-05-27 12:42:38 +0200

    revamped nav-bar

--- boozebuddiez/src/components/Navigation.vue
+++ boozebuddiez/src/components/Navigation.vue
@@ -1,12 +1,13 @@
 <template>
   <div class="toolbar">
-    <img alt="booze logo" class = "logo routerlink" src="../assets/images/boozebuddieslogo.png" />
-    <router-link class = "routerlink" to="/">Home </router-link>
-    <router-link class = "routerlink" to="/about">About </router-link>
-    <router-link class = "routerlink" v-if="$auth.isAuthenticated" to="/profile">Profile </router-link>
-    <router-link class = "routerlink" v-if="$auth.isAuthenticated" to="/map">Map </router-link>
-    <router-link class = "routerlink" v-if="$auth.isAuthenticated" to="/rating">Rating </router-link>
-    <button class = "routerlink" v-if="$auth.isAuthenticated" v-on:click="this.$auth.logout">Logout</button>
+    <img alt="booze logo" class="logo" src="../assets/images/boozebuddieslogo.png" />
+    <h2 class="logotext">BoozeBuddies</h2>
+    <button class="routerlink logoutbutton" v-if="$auth.isAuthenticated" v-on:click="this.$auth.logout">Logout</button>
+    <router-link class="routerlink" v-if="$auth.isAuthenticated" to="/rating">Rating</router-link>
+    <router-link class="routerlink" v-if="$auth.isAuthenticated" to="/map">Map</router-link>
+    <router-link class="routerlink" v-if="$auth.isAuthenticated" to="/profile">Profile</router-link>
+    <router-link class="routerlink" to="/about">About</router-link>
+    <router-link class="routerlink" to="/">Home</router-link>
   </div>
 </template>
 
@@ -28,7 +29,7 @@ export default {
   height: 60px;
   align-items: center;
   background-color: blanchedalmond;
-  color: lightgray;
+  color: grey;
   font-weight: 600;
 }
 .toolbar img {
@@ -41,13 +42,26 @@ export default {
 .toolbar #twitter-logo:hover {
   opacity: 0.8;
 }
-.routerlink{
+.routerlink {
   position: relative;
-  top: 50%;
+  top: 40%;
+  margin-right: 10px;
+  font-size: 20px;
+  color: grey !important;
+  float: right;
 }
-.logo{
-    border-style: none;
-    height: 100%;
-    width: auto;
+.logo {
+  border-style: none;
+  height: 100%;
+  width: auto;
+  float: left;
+}
+.logotext {
+  float: left;
+  border-bottom: 2px solid currentColor;
+  color: grey;
+}
+.logoutbutton {
+  text-decoration: underline;
 }
 </style>
