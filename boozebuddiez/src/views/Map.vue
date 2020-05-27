64ca8e4dd595c83c6634860dce9dff569f537316
MarioHendriks <43466121+MarioHendriks@users.noreply.github.com>
2020-05-27 10:13:24 +0200

    .

---
+++ boozebuddiez/src/views/Map.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>  
+        <div id="app">
+            <mapbox
+            access-token="pk.eyJ1IjoibWFyaW8wMjYiLCJhIjoiY2thcDFvZ2ZtMDFnaTJ6cGFiajRwN3didyJ9.CYOzUb9FrBkpqg-NcFbJfA"
+            :map-options="{
+                style: 'mapbox://styles/mapbox/light-v9',
+                center: [-96, 37.8],
+                zoom: 3,
+            }"
+            />
+        </div>
+  </div>
+</template>
+
+<script>
+import Mapbox from 'mapbox-gl-vue'
+export default {
+    name: "Map",
+    components:{
+        Mapbox
+    }
+
+}
+</script>
+
+<style>
+#map {
+  width: 100%;
+  height: 500px;
+}
+</style>
