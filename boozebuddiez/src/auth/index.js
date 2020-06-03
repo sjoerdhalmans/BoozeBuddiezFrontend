03d7ce09f8de0b8517f4a19a1367aa8cf9479b73
sjoerdhalmans <43236006+sjoerdhalmans@users.noreply.github.com>
2020-06-03 09:42:06 +0200

    now routing to map

--- boozebuddiez/src/auth/index.js
+++ boozebuddiez/src/auth/index.js
@@ -14,7 +14,7 @@ export const getInstance = () => instance;
 /** Creates an instance of the Auth0 SDK. If one has already been created, it returns that instance */
 export const useAuth0 = ({
   onRedirectCallback = DEFAULT_REDIRECT_CALLBACK,
-  redirectUri = window.location.origin + "/rating",
+  redirectUri = window.location.origin + "/map",
   ...options
 }) => {
   if (instance)
