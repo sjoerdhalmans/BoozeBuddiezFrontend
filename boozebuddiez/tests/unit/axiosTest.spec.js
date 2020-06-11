8adf83dae975cac40320ff54e7324d8f42f754d3
WiebevanHerwijnen <32171131+WiebevanHerwijnen@users.noreply.github.com>
2020-06-11 15:31:56 +0200

    Update axiosTest.spec.js

--- boozebuddiez/tests/unit/axiosTest.spec.js
+++ boozebuddiez/tests/unit/axiosTest.spec.js
@@ -1,10 +1,23 @@
-import { mount, shallowMount } from '@vue/test-utils'
-import Rating from '../../src/views/Rating.vue'
+import { mount, shallowMount ,createLocalVue} from '@vue/test-utils'
+import resultCard from '../../src/components/sidebar/MapSideBar/resultCard.vue'
 import store from '../../src/store'
+import Vuex from 'vuex'
 
-const wrapper = mount(Rating);
+
 describe('test.vue', () =>{
-    test('setup correctly', () =>{
-        expect(wrapper.html()).toContain('My bar ratings')
+    const localVue = createLocalVue()
+    localVue.use(Vuex)
+    test('Is a vue instance', () =>{
+        const wrapper = mount(resultCard, {
+            store,
+            localVue,
+            bar: {
+              name: 'yeet',
+              zipcode: '123',
+              adress: 'teststraat 3',
+              telephoneNumber:'1236456978',
+            }
+          })
+        expect(wrapper.isVueInstance()).toBeTruthy()
     })
 })
