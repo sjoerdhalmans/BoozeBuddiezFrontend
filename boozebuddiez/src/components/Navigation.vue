6838c0d6ef087aa8f6fd8c04ec7fdac9ce0fcf3f
coecomber <joostvherwaarden@hotmail.com>
2020-06-22 10:13:05 +0200

    Navigation.vue mobile view update

--- boozebuddiez/src/components/Navigation.vue
+++ boozebuddiez/src/components/Navigation.vue
@@ -7,7 +7,7 @@
     <router-link class="routerlink" v-if="$auth.isAuthenticated" to="/map">Map</router-link>
     <!-- <router-link class="routerlink" v-if="$auth.isAuthenticated" to="/profile">Profile</router-link>
     <router-link class="routerlink" v-if="$auth.isAuthenticated" to="/about">About</router-link> -->
-    <br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
+    <br /><br /><br /><br /><br />
     <!-- <router-link class="routerlink" to="/">Home</router-link> -->
   </div>
 </template>
@@ -22,46 +22,68 @@ export default {
 </script>
 
 <style>
-.toolbar {
-  position: relative;
-  top: 0;
-  left: 0;
-  right: 0;
-  height: auto;
-  align-items: center;
-  background-color: blanchedalmond;
-  color: grey;
-  font-weight: 600;
+@media (min-width:700px) {
+  .toolbar {
+    position: relative;
+    top: 0;
+    left: 0;
+    right: 0;
+    height: auto;
+    align-items: center;
+    background-color: blanchedalmond;
+    color: grey;
+    font-weight: 600;
+  }
+  .toolbar img {
+    margin: 0 16px;
+  }
+  .toolbar #twitter-logo {
+    height: 40px;
+    margin: 0 16px;
+  }
+  .toolbar #twitter-logo:hover {
+    opacity: 0.8;
+  }
+  .routerlink {
+    position: relative;
+    top: 40%;
+    margin-right: 10px;
+    font-size: 20px;
+    float: right;
+  }
+  .logo {
+    border-style: none;
+    height: 80px;
+    width: auto;
+    float: left;
+  }
+  .logotext {
+    float: left;
+    border-bottom: 2px solid currentColor;
+    color: grey;
+  }
+  .logoutbutton {
+    text-decoration: underline;
+  }
 }
-.toolbar img {
-  margin: 0 16px;
-}
-.toolbar #twitter-logo {
-  height: 40px;
-  margin: 0 16px;
-}
-.toolbar #twitter-logo:hover {
-  opacity: 0.8;
-}
-.routerlink {
-  position: relative;
-  top: 40%;
-  margin-right: 10px;
-  font-size: 20px;
-  float: right;
-}
-.logo {
+@media (max-width:700px) {
+   .logo {
   border-style: none;
-  height: 80px;
-  width: auto;
+  height: auto;
+  max-height: 85px;
+  width: 25%;
   float: left;
-}
-.logotext {
+  }
+  .logotext {
   float: left;
+  width: 60%;
   border-bottom: 2px solid currentColor;
   color: grey;
-}
-.logoutbutton {
-  text-decoration: underline;
+  font-size: 26px;
+  }
+  .routerlink {
+  visibility: hidden;
+  }
+
 }
 </style>
