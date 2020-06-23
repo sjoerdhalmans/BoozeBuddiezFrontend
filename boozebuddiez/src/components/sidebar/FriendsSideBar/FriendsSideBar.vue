a05357c62eccff14ec47a1397182b2eb72cb4796
MarioHendriks <43466121+MarioHendriks@users.noreply.github.com>
2020-06-23 12:18:37 +0200

    Update FriendsSideBar.vue

--- boozebuddiez/src/components/sidebar/FriendsSideBar/FriendsSideBar.vue
+++ boozebuddiez/src/components/sidebar/FriendsSideBar/FriendsSideBar.vue
@@ -4,7 +4,7 @@
             <div class="col-12 friends">My Friends</div>
         </div>
         <div v-for="friend in this.friends" :key="friend.id">
-            <div>{{friend.userTwoId.name}}.</div>
+            <div>{{friend.name}}.</div>
         </div>
         <v-app>
             <AddNewFriendModel></AddNewFriendModel>
@@ -18,6 +18,11 @@ import axios from 'axios';
 import AddNewFriendModel from '../../sidebar/FriendsSideBar/AddNewFriendModel.vue'
 export default {
     name:"FriendsSideBar",
+    data(){
+        return{
+            array:[]
+        }
+    },
         computed:{
         friends(){
             return this.$store.getters.getFriends
@@ -26,7 +31,14 @@ export default {
         mounted(){
         axios.get('http://217.101.44.31:8082/api/public/friend/getFriendsByUserId/'+this.$store.getters.getUser.id)
       .then(response => (
-        this.$store.dispatch("SaveFriends", response.data.relationships)
+        response.data.relationships.forEach(relationships => {
+            if(relationships.userOneId.id === this.$store.getters.getUser.id){
+                return this.array.push(relationships.userTwoId)
+            }
+            return this.array.push(relationships.userOneId)
+        }),
+        console.log(this.array),
+        this.$store.dispatch("SaveFriends", this.array)
         ))
     },
     components:{
